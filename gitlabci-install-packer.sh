94c28fc8fb50aa177e82e3bff6e01ebc96f1a312
Fadli Nurhasan <fadli.nurhasan@gmail.com>
2019-08-08 18:18:37 +0700

    update packer version for auto accept chef_license

--- gitlabci-install-packer.sh
+++ gitlabci-install-packer.sh
@@ -3,6 +3,6 @@ set -e
 if [ ! -e packer/packer ]; then
   sudo apt update
   sudo apt install wget unzip -yqq
-  wget https://releases.hashicorp.com/packer/1.3.5/packer_1.3.5_linux_amd64.zip
-  unzip packer_1.3.5_linux_amd64.zip -d packer
+  wget https://releases.hashicorp.com/packer/1.4.2/packer_1.4.2_linux_amd64.zip
+  unzip packer_1.4.2_linux_amd64.zip -d packer
 fi
