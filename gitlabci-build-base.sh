a5c9086a682a644f42dd8907927a38e11b16aac4
Tara Baskara <tara.baskara@gmail.com>
2019-10-23 15:49:20 +0700

    modify build base script to use template file

--- gitlabci-build-base.sh
+++ gitlabci-build-base.sh
@@ -1,5 +1,9 @@
 set -x
 set -e
-CONFIG_PATH=${1:-./packer.json}
+if [ -z "$TEMPLATE_FILE" ]; then 
+  CONFIG_PATH=${1:-./packer.json};
+else 
+  CONFIG_PATH=$TEMPLATE_FILE;  
+fi
 ./packer/packer build $CONFIG_PATH
 lxc image list
