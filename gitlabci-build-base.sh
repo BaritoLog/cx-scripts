63db56dba41f6fac61f8c1c46cb16e8a72fd8740
Tara Baskara <tara.baskara@gmail.com>
2019-10-23 16:17:48 +0700

    add remote stream for ubuntu-minimal

--- gitlabci-build-base.sh
+++ gitlabci-build-base.sh
@@ -1,5 +1,6 @@
 set -x
 set -e
+lxc remote add --protocol simplestreams ubuntu-minimal https://cloud-images.ubuntu.com/minimal/releases/
 if [ -z "$TEMPLATE_FILE" ]; then 
   CONFIG_PATH=${1:-./packer.json};
 else 
