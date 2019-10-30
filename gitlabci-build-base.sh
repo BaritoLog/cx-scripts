e3da5bfc0de1bf1a8eeb5804161a1083eff9e7d2
Eka Risky F (ekarisky) <ekarisky@gmail.com>
2019-10-30 14:28:53 +0700

    ignore error when add lxc remote

--- gitlabci-build-base.sh
+++ gitlabci-build-base.sh
@@ -1,10 +1,10 @@
 set -x
 set -e
-lxc remote add --protocol simplestreams ubuntu-minimal https://cloud-images.ubuntu.com/minimal/releases/
-if [ -z "$TEMPLATE_FILE" ]; then 
-  CONFIG_PATH=${1:-./packer.json};
-else 
-  CONFIG_PATH=$TEMPLATE_FILE;  
+lxc remote add --protocol simplestreams ubuntu-minimal https://cloud-images.ubuntu.com/minimal/releases/ || true
+if [ -z "$TEMPLATE_FILE" ]; then
+  CONFIG_PATH=${1:-./packer.json}
+else
+  CONFIG_PATH=$TEMPLATE_FILE
 fi
 ./packer/packer build $CONFIG_PATH
 lxc image list
