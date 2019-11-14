c18f07dc55f085d0ec72dad40943cca152901de8
Eka Risky F (ekarisky) <ekarisky@gmail.com>
2019-11-14 16:21:26 +0700

    make image public when copy to barito registry

--- gitlabci-copy-image.sh
+++ gitlabci-copy-image.sh
@@ -1,4 +1,4 @@
 set -x
 set -e
-lxc image copy $OUTPUT_IMAGE $IMAGE_REGISTRY: --alias $OUTPUT_IMAGE
+lxc image copy $OUTPUT_IMAGE $IMAGE_REGISTRY: --alias $OUTPUT_IMAGE --public
 lxc image delete $OUTPUT_IMAGE
